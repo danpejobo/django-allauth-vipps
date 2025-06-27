da03ff5895c45a7ed990d9a7714586f06abd455f
danpejobo <danpe_jobo@hotmail.com>
2025-06-27 11:16:58 +0000

    fix: still errors

--- tests/urls.py
+++ tests/urls.py
@@ -2,24 +2,41 @@
 
 from django.urls import path, include
 from dj_rest_auth.registration.views import SocialLoginView
-from vipps_auth.views import VippsOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
+# Import the adapters we need
+from vipps_auth.views import VippsOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+
+
+# --- The Vipps View (Subclassing is Required) ---
 class VippsLogin(SocialLoginView):
     adapter_class = VippsOAuth2Adapter
     client_class = OAuth2Client
-    # Define the callback_url used during the test flow.
-    callback_url = "http://localhost/callback"
+    callback_url = "http://localhost/callback/vipps"
+
+# --- The Google View (Subclassing is Required) ---
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = "http://localhost/callback/google"
 
 
 urlpatterns = [
-    # The API endpoint our test will hit
+    # Vipps API endpoint using the required subclass
     path(
         "api/v1/auth/social/vipps/",
         VippsLogin.as_view(),
         name="vipps_login_api"
     ),
+
+    # Google API endpoint using the required subclass
+    path(
+        "api/v1/auth/social/google/",
+        GoogleLogin.as_view(),
+        name="google_login_api"
+    ),
     
-    # The browser-flow URLs from our package
+    # Browser-flow URLs from our package
     path("accounts/", include("vipps_auth.urls")),
 ]
